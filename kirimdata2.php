30ad5891d4e12cf87541fb43a51dbd8341e27e8e
gen-em29 <genyus073@gmail.com>
2025-04-28 22:18:29 +0700

    Delete kirimdata2.php

--- kirimdata2.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$koneksi = mysqli_connect("localhost", "root",
-	 "", "website-coba");
-
-	$nilai_sensor = $_GET['sensor'];
-
-	//update nilai sensor
-	mysqli_query ($koneksi, "UPDATE tb_sensor SET sensor='$nilai_sensor'")
- ?>
